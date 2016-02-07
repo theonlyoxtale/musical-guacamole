6aa98ec216a4cc74c9f6f0700272aea23aa795a7
theonlyoxtale <theonlyoxtale@gmail.com>
2016-02-07 18:03:05 -0500

    adding initial build w/character movement and rotation based on mouse position

---
+++ PLAYGROUND/first-grade/first-grade-top-down/second-grade-top-down/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerControllerScript : MonoBehaviour
+{
+    //Player variables
+    public float moveSpeed = 3f;
+    public Camera currentCam;
+
+    //System variables
+
+    // Update is called once per frame
+    void Update()
+    {
+        //controls Player movement
+        transform.Translate(Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed, 0f, Input.GetAxisRaw("Vertical") * Time.deltaTime * moveSpeed);
+
+        //mouse Look script 
+        Plane plane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+        float dist;
+        if (plane.Raycast (ray, out dist)) {
+            transform.LookAt (ray.GetPoint(dist));
+        }
+    }
+    }
