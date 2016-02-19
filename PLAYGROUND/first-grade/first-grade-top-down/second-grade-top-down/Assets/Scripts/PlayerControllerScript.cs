846a8997e8884a4480e8493ad12bcbaa5456b336
theonlyoxtale <theonlyoxtale@gmail.com>
2016-02-18 20:30:59 -0500

    Updated player movement
    smoothed out the player movement and added a running feature

--- PLAYGROUND/first-grade/first-grade-top-down/second-grade-top-down/Assets/Scripts/PlayerControllerScript.cs
+++ PLAYGROUND/first-grade/first-grade-top-down/second-grade-top-down/Assets/Scripts/PlayerControllerScript.cs
@@ -5,24 +5,36 @@ using System.Collections;
 public class PlayerControllerScript : MonoBehaviour
 {
     //Player variables
-    public float moveSpeed = 3f;
-    public Camera currentCam;
-    public float zDistance = 10f;
+    public float walkingSpeed_f = 4f;
+    public float runSpeed_f = 8f;
+    public float rotationSpeed_f = 450f;
 
     //System variables
+    private Quaternion targetRotation;
+
+    //Component variables
+    private CharacterController controller;
+
+    void Start(){
+        controller = GetComponent<CharacterController>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //controls Player movement
-        transform.Translate(Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed, 0f, Input.GetAxisRaw("Vertical") * Time.deltaTime * moveSpeed);
+        Vector3 playerInput_v = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-        // mouse Look script
-        Plane plane = new Plane(Vector3.up, transform.position);
-        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-        float dist;
-        if (plane.Raycast (ray, out dist)) {
-            transform.LookAt (ray.GetPoint(dist));
+        if(playerInput_v != Vector3.zero){
+            //takes in a direction and makes the player look in that direction
+            targetRotation = Quaternion.LookRotation(playerInput_v);
+            transform.eulerAngles = Vector3.up*Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed_f*Time.deltaTime);
         }
+
+        // setting up player movement
+        Vector3 motion = playerInput_v;
+        motion *= (Mathf.Abs(playerInput_v.x) == 1 && Mathf.Abs(playerInput_v.z) == 1) ? .7f : 1;
+        motion *= (Input.GetButton("Run")?runSpeed_f:walkingSpeed_f);
+        motion += Vector3.up * -8;
+        controller.Move(motion * Time.deltaTime);
     }
-    }
+}
