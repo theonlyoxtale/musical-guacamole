2b4ae39438c030b1a98154e2908018adb88dab06
theonlyoxtale <theonlyoxtale@gmail.com>
2016-02-19 22:29:07 -0500

    Added code for the camera to follow the player

---
+++ PLAYGROUND/first-grade/first-grade-top-down/second-grade-top-down/Assets/Scripts/GameCameraScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCameraScript : MonoBehaviour {
+
+    private Vector3 cameraTarget;
+    private Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+        //using in Update method takes up resources
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+	// Update is called once per frame
+	void Update () {
+    cameraTarget = new Vector3(target.position.x, transform.position.y, target.position.z);
+    transform.position = Vector3.Lerp(transform.position, cameraTarget, Time.deltaTime*8);
+	}
+}
