4bdbc0d7e3707d24c4c0d9fed8dda94a9d4ea883
theonlyoxtale <theonlyoxtale@gmail.com>
2016-03-05 22:11:18 -0500

    Updated the PlayerControllerScript

--- PLAYGROUND/first-grade/first-grade-top-down/second-grade-top-down/Assets/Scripts/PlayerControllerScript.cs
+++ PLAYGROUND/first-grade/first-grade-top-down/second-grade-top-down/Assets/Scripts/PlayerControllerScript.cs
@@ -20,21 +20,38 @@ public class PlayerControllerScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update(){
+        ControlMouse();
+        //ControlWASD();
+    }
+
+    void ControlMouse()
     {
-        Vector3 playerInput_v = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+      //Getting the mouse position
+      
+      Vector3 playerInput_v = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+      // setting up player movement
+      Vector3 motion = playerInput_v;
+      motion *= (Mathf.Abs(playerInput_v.x) == 1 && Mathf.Abs(playerInput_v.z) == 1) ? .7f : 1;
+      motion *= (Input.GetButton("Run")?runSpeed_f:walkingSpeed_f);
+      motion += Vector3.up * -8;
+      controller.Move(motion * Time.deltaTime);
+    }
 
-        if(playerInput_v != Vector3.zero){
-            //takes in a direction and makes the player look in that direction
-            targetRotation = Quaternion.LookRotation(playerInput_v);
-            transform.eulerAngles = Vector3.up*Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed_f*Time.deltaTime);
-        }
+    void ControlWASD(){
+      Vector3 playerInput_v = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-        // setting up player movement
-        Vector3 motion = playerInput_v;
-        motion *= (Mathf.Abs(playerInput_v.x) == 1 && Mathf.Abs(playerInput_v.z) == 1) ? .7f : 1;
-        motion *= (Input.GetButton("Run")?runSpeed_f:walkingSpeed_f);
-        motion += Vector3.up * -8;
-        controller.Move(motion * Time.deltaTime);
+      if(playerInput_v != Vector3.zero){
+          //takes in a direction and makes the player look in that direction
+          targetRotation = Quaternion.LookRotation(playerInput_v);
+          transform.eulerAngles = Vector3.up*Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed_f*Time.deltaTime);
+      }
+
+      // setting up player movement
+      Vector3 motion = playerInput_v;
+      motion *= (Mathf.Abs(playerInput_v.x) == 1 && Mathf.Abs(playerInput_v.z) == 1) ? .7f : 1;
+      motion *= (Input.GetButton("Run")?runSpeed_f:walkingSpeed_f);
+      motion += Vector3.up * -8;
+      controller.Move(motion * Time.deltaTime);
     }
 }
